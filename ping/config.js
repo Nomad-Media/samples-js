d07c7e411e1ce65d9e7aa6dcf47a87e7e4ef7443
scott <slfaludi@gmail.com>
2024-02-21 16:41:23 -0800

    removed config.js

--- ping/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    "username": "username",
-    "password": "password",
-    "serviceApiUrl": "serviceApiUrl",
-    "apiType": "admin",
-    "debugMode": true
-};
-
-export default config;
