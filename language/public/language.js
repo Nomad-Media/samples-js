71ac6cb17265c34fa370e75b6237c2e4eb8ca297
scott <slfaludi@gmail.com>
2024-05-18 06:15:56 -0700

    language sample

---
+++ language/public/language.js
@@ -0,0 +1,300 @@
+const CREATE_FORM = document.getElementById("createForm");
+const GET_FORM = document.getElementById("getForm");
+const UPDATE_FORM = document.getElementById("updateForm");
+const DELETE_FORM = document.getElementById("deleteForm");
+
+const CREATE_CONTENT_LANGUAGE_PROPERTIES_DIV = document.getElementById("createContentLanguagePropertiesDiv");
+const UPDATE_CONTENT_LANGUAGE_PROPERTIES_DIV = document.getElementById("updateContentLanguagePropertiesDiv");
+
+const CREATE_CONTENT_CONTENT_DEFINITION_ID = document.getElementById("createContentContentDefinitionId");
+const CREATE_CONTENT_MASTER_ID = document.getElementById("createContentMasterId");
+const CREATE_CONTENT_LANGUAGE_ID = document.getElementById("createContentLanguageId");
+const UPDATE_CONTENT_CONTENT_DEFINITION_ID = document.getElementById("updateContentContentDefinitionId");
+const UPDATE_CONTENT_MASTER_ID = document.getElementById("updateContentMasterId");
+const UPDATE_CONTENT_LANGUAGE_ID = document.getElementById("updateContentLanguageId");
+
+const CREATE_CONTENT_ADD_PROPERTIES_BUTTON = document.getElementById("createContentAddPropertiesButton");
+const CREATE_CONTENT_SUBMIT_BUTTON = document.getElementById("createContentSubmitButton");
+const UPDATE_CONTENT_ADD_PROPERTIES_BUTTON = document.getElementById("updateContentAddPropertiesButton");
+const UPDATE_CONTENT_SUBMIT_BUTTON = document.getElementById("updateContentSubmitButton");
+
+const CREATE_CONTENT_LANGUAGE_SELECT = document.getElementById("createContentLanguage");
+const GET_CONTENT_LANGUAGE_SELECT = document.getElementById("getContentLanguage");
+const UPDATE_CONTENT_LANGUAGE_SELECT = document.getElementById("updateContentLanguage");
+const DELETE_CONTENT_LANGUAGE_SELECT = document.getElementById("deleteContentLanguage");
+
+await getLanguages()
+
+async function getLanguages()
+{
+    const LANGUAGES = await sendRequest("/getLanguages", "GET");
+    if (LANGUAGES)
+    {
+        for (let language of LANGUAGES)
+        {
+            const OPTION = document.createElement("option");
+            OPTION.value = language.id;
+            OPTION.text = language.identifiers.title;
+            CREATE_CONTENT_LANGUAGE_SELECT.appendChild(OPTION);
+            GET_CONTENT_LANGUAGE_SELECT.appendChild(OPTION.cloneNode(true));
+            UPDATE_CONTENT_LANGUAGE_SELECT.appendChild(OPTION.cloneNode(true));
+            DELETE_CONTENT_LANGUAGE_SELECT.appendChild(OPTION.cloneNode(true));
+        }
+    }
+
+    $(CREATE_CONTENT_LANGUAGE_SELECT).select2();
+    $(GET_CONTENT_LANGUAGE_SELECT).select2();
+    $(UPDATE_CONTENT_LANGUAGE_SELECT).select2();
+    $(DELETE_CONTENT_LANGUAGE_SELECT).select2();
+}
+
+CREATE_CONTENT_ADD_PROPERTIES_BUTTON.addEventListener("click", async () => {
+    await addProperties(CREATE_CONTENT_CONTENT_DEFINITION_ID.value,
+        CREATE_CONTENT_MASTER_ID.value, CREATE_CONTENT_LANGUAGE_SELECT.value,
+        CREATE_CONTENT_LANGUAGE_PROPERTIES_DIV, 
+        CREATE_CONTENT_ADD_PROPERTIES_BUTTON, CREATE_CONTENT_SUBMIT_BUTTON,
+        "create");
+});
+
+UPDATE_CONTENT_ADD_PROPERTIES_BUTTON.addEventListener("click", async () => {
+    await addProperties(UPDATE_CONTENT_CONTENT_DEFINITION_ID.value,
+        UPDATE_CONTENT_MASTER_ID.value, UPDATE_CONTENT_LANGUAGE_SELECT.value,
+        UPDATE_CONTENT_LANGUAGE_PROPERTIES_DIV, 
+        UPDATE_CONTENT_ADD_PROPERTIES_BUTTON, UPDATE_CONTENT_SUBMIT_BUTTON,
+        "update");
+});
+
+async function addProperties(CONTENT_DEFINITION_ID, MASTER_ID, LANGUAGE_ID, DIV,
+    ADD_PROPERTIES_BUTTON, SUBMIT_BUTTON, ACTION)
+{
+    ADD_PROPERTIES_BUTTON.hidden = true;
+
+    const PROPERTIES_FORM_DATA = new FormData();
+    PROPERTIES_FORM_DATA.append("contentDefinitionId", CONTENT_DEFINITION_ID);
+
+    const PROPERTIES = await sendRequest("/getProperties", "POST", PROPERTIES_FORM_DATA);
+
+    let contentId = null;
+    if (ACTION === "create")
+    {
+        contentId = MASTER_ID
+    }
+    else
+    {
+        const FORM_DATA = new FormData();
+        FORM_DATA.append("contentDefinitionId", CONTENT_DEFINITION_ID);
+        FORM_DATA.append("languageId", LANGUAGE_ID);
+
+        const CONTENTS = await sendRequest("/getContents", "POST", FORM_DATA);
+        try
+        {
+            contentId = CONTENTS.find(content => content.masterId === MASTER_ID).id;
+        } 
+        catch (error)
+        {
+            console.error("Master Id not found in content definition");
+        }
+    }
+
+    const CONTENT_FORM_DATA = new FormData();
+    CONTENT_FORM_DATA.append("contentId", contentId);
+    CONTENT_FORM_DATA.append("contentDefinitionId", CONTENT_DEFINITION_ID);
+
+    const CONTENT_DATA = await sendRequest("/getContentData", "POST", CONTENT_FORM_DATA);
+
+    const PROPERTIES_DIV = document.createElement("div");
+    PROPERTIES_DIV.classList.add("properties");
+    
+    for (let contentField of PROPERTIES.contentFields)
+    {
+        const PROPERTY = contentField.properties;
+
+        const PROPERTY_DIV = document.createElement("div");
+
+        const LABEL = document.createElement("label");
+        LABEL.textContent = PROPERTY.title;
+        PROPERTY_DIV.appendChild(LABEL);
+
+        let camelPropertyTitle = PROPERTY.title.replace(/ /g, "");
+        camelPropertyTitle = camelPropertyTitle[0].toLowerCase() + camelPropertyTitle.slice(1);
+
+        if ((PROPERTY.fieldId.description === "Short Text" || PROPERTY.fieldId.description === "Number") && contentField.isInEditorForm)
+        {
+            const INPUT = document.createElement("input");
+            INPUT.type = "text";
+            INPUT.id = camelPropertyTitle;
+            if (CONTENT_DATA && CONTENT_DATA.properties[camelPropertyTitle]) INPUT.value = CONTENT_DATA.properties[camelPropertyTitle];
+
+            PROPERTY_DIV.appendChild(INPUT);
+            PROPERTIES_DIV.appendChild(PROPERTY_DIV);
+        }
+        else if (PROPERTY.fieldId.description === "Long Text" && contentField.isInEditorForm)
+        {
+            const TEXTAREA = document.createElement("textarea");
+            TEXTAREA.id = camelPropertyTitle;
+
+            if (CONTENT_DATA && CONTENT_DATA.properties[camelPropertyTitle]) TEXTAREA.value = CONTENT_DATA.properties[camelPropertyTitle];
+
+            PROPERTY_DIV.appendChild(TEXTAREA);
+            PROPERTIES_DIV.appendChild(PROPERTY_DIV);
+        }
+        else if ((PROPERTY.fieldId.description === "Lookup Multi-Select Chip View" || 
+            PROPERTY.fieldId.description === "Related Content Definition") && contentField.isInEditorForm)
+        {
+            const SELECT = document.createElement("select");
+            SELECT.multiple = PROPERTY.fieldId.description === "Lookup Multi-Select Chip View";
+            SELECT.id = camelPropertyTitle;
+            
+            const CONTENT_DEFINITION_ID = PROPERTY.fieldId.description === "Lookup Multi-Select Chip View" ?
+                PROPERTY.lookupDropdownProperties.lookupKey :
+                PROPERTY.relatedContentDefinition.id;
+
+            const FORM_DATA = new FormData();
+            FORM_DATA.append("contentDefinitionId", CONTENT_DEFINITION_ID);
+            FORM_DATA.append("languageId", LANGUAGE_ID);
+
+            const OPTIONS = await sendRequest("/getContents", "POST", FORM_DATA);
+            
+            for (let option of OPTIONS)
+            {
+                const OPTION = document.createElement("option");
+                OPTION.value = option.id;
+                OPTION.text = option.title;
+
+                if (CONTENT_DATA && CONTENT_DATA.properties[camelPropertyTitle]) 
+                {
+                    //debugger;
+                    if (PROPERTY.fieldId.description === "Lookup Multi-Select Chip View")
+                    {
+                        for (let selectedOption of CONTENT_DATA.properties[camelPropertyTitle])
+                        {
+                            if (selectedOption.id === option.id)
+                            {
+                                OPTION.selected = true;
+                            }
+                        }
+                    }
+                    else if (CONTENT_DATA.properties[camelPropertyTitle].id === option.id)
+                    {
+                        OPTION.selected = true;
+                    }
+                }
+
+                SELECT.appendChild(OPTION);
+            }
+
+            PROPERTY_DIV.appendChild(SELECT);
+            PROPERTIES_DIV.appendChild(PROPERTY_DIV);
+
+            $(SELECT).select2();
+        }
+        else if (PROPERTY.fieldId.description === "Asset Selector" && contentField.isInEditorForm)
+        {
+            LABEL.textContent += " Id";
+
+            const INPUT = document.createElement("input");
+            INPUT.type = "text";
+            INPUT.id = camelPropertyTitle;
+
+            if (CONTENT_DATA && CONTENT_DATA.properties[camelPropertyTitle]) INPUT.value = CONTENT_DATA.properties[camelPropertyTitle].id;
+
+            PROPERTY_DIV.appendChild(INPUT);
+            PROPERTIES_DIV.appendChild(PROPERTY_DIV);
+        }
+    }
+    
+    DIV.appendChild(PROPERTIES_DIV);
+
+    SUBMIT_BUTTON.hidden = false;
+}
+
+CREATE_FORM.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const FORM_DATA = getElements(CREATE_FORM);
+    await sendRequest("/createContent", "POST", FORM_DATA);
+});
+
+GET_FORM.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const FORM_DATA = getElements(GET_FORM);
+    await sendRequest("/getContent", "POST", FORM_DATA);
+});
+
+UPDATE_FORM.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const FORM_DATA = getElements(UPDATE_FORM);
+    await sendRequest("/updateContent", "POST", FORM_DATA);
+});
+
+DELETE_FORM.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const FORM_DATA = getElements(DELETE_FORM);
+    await sendRequest("/deleteContent", "POST", FORM_DATA);
+});
+
+function getElements(FORM)
+{
+    const FORM_DATA = new FormData();
+    for (let input of FORM)
+    {
+        if (input.tagName === "SELECT") {
+            const SELECTED_OPTIONS = []
+            for (let element of input) {
+                if (element.selected) {
+                    if (element.value.trim().toLowerCase() === element.label.trim().toLowerCase()) {
+                        if (input.id) {
+                            FORM_DATA.append(input.id, element.value);
+                        } else {
+                            FORM_DATA.append(input.name, element.value);
+                        }
+                    } else {
+                        SELECTED_OPTIONS.push({ id: element.value, description: element.label });
+                    }
+                }
+            }
+            if (input.multiple)
+            {
+                FORM_DATA.append(input.id, JSON.stringify(SELECTED_OPTIONS));
+            }
+            else
+            {
+                FORM_DATA.append(input.id, JSON.stringify(SELECTED_OPTIONS[0]));
+            }
+        }
+        else if (input.tagName === "INPUT")
+        {
+            if (input.id) {
+                FORM_DATA.append(input.id, input.value);
+            } else {
+                FORM_DATA.append(input.name, input.value);
+            }
+        }
+    }
+    return FORM_DATA;
+}
+
+async function sendRequest(PATH, METHOD, BODY)
+{
+    try
+    {
+        const REQUEST = { method: METHOD };
+        if (BODY) REQUEST["body"] = BODY;
+        const RESPONSE = await fetch(PATH, REQUEST);
+
+        if (RESPONSE.ok)
+        {
+            const DATA = await RESPONSE.json();
+            if (DATA) return DATA;
+        }
+        else
+        {
+            const INFO = await RESPONSE.json();
+            console.error(JSON.stringify(INFO, null, 4));
+            console.error("HTTP-Error: " + RESPONSE.status);
+        }
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+}
---
+++ language/server.js
@@ -0,0 +1,342 @@
+const LANGUAGE_CONTENT_DEFINITION_ID = "e4b10c04-1878-4830-a115-e42d52705059";
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import NomadMediaSDK from "@nomad-media/full";
+import config from "./config.js";
+const NomadSDK = new NomadMediaSDK(config);
+
+import express from 'express';
+import multer from 'multer';
+
+const app = express();
+const upload = multer();
+const port = 4200;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/language.html');
+});
+
+app.get('/getLanguages', async (req, res) => {
+    try
+    {
+        const LANGUAGES = [];
+        let offset = 0;
+        while (true)
+        {
+            const SEARCH_INFO = await NomadSDK.search(null, offset, null, 
+                [
+                    {
+                        fieldName: "contentDefinitionId",
+                        operator: "Equals",
+                        values: LANGUAGE_CONTENT_DEFINITION_ID,
+                    },
+                    {
+                        fieldName: "languageId",
+                        operator: "Equals",
+                        values: "c66131cd-27fc-4f83-9b89-b57575ac0ed8"
+                    },
+                    {
+                        fieldName: "active",
+                        operator: "Equals",
+                        values: true
+                    }
+                ], null, null, null, null, true, null);
+
+            if (!SEARCH_INFO) return [];
+            LANGUAGES.push(...SEARCH_INFO.items);
+
+            ++offset;
+
+            if (SEARCH_INFO.items.length < 100) break;
+        }
+        return res.status(200).json(LANGUAGES);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/getProperties', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT_DEFINITION_ID = req.body.contentDefinitionId;
+
+        const PROPERTIES = await NomadSDK.getContentDefinition(CONTENT_DEFINITION_ID);
+        return res.status(200).json(PROPERTIES);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/getContents', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT_DEFINITION_ID = req.body.contentDefinitionId;
+        const LANGUAGE_ID = req.body.languageId;
+
+        const CONTENTS = [];
+        let offset = 0;
+
+        while (true)
+        {
+            const SEARCH_INFO = await NomadSDK.search(null, offset, null, 
+                [
+                    {
+                        fieldName: "contentDefinitionId",
+                        operator: "Equals",
+                        values: CONTENT_DEFINITION_ID,
+                    },
+                    {
+                        fieldName: "languageId",
+                        operator: "Equals",
+                        values: LANGUAGE_ID
+                    }
+                ], null, null, null, null, true, null);
+
+            if (!SEARCH_INFO || SEARCH_INFO.items.length === 0) break;
+            CONTENTS.push(...SEARCH_INFO.items);
+
+            ++offset;
+
+            if (SEARCH_INFO.items.length < 100) break;
+        }
+
+        return res.status(200).json(CONTENTS);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/getContentData', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT = await NomadSDK.getContent(req.body.contentId, req.body.contentDefinitionId);
+        return res.status(200).json(CONTENT);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/createContent', upload.none(), async (req, res) => {
+    try
+    {
+        const PROPERTIES = {};
+        const GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+        for (const KEY in req.body)
+        {
+            let keyData = checkString(req.body[KEY]);
+            req.body[KEY] = keyData;
+
+            if (KEY.includes("createContent"))
+            {
+                let correctedKey = KEY.replace("createContent", "");
+                correctedKey = correctedKey.charAt(0).toLowerCase() + correctedKey.slice(1);
+                req.body[correctedKey] = typeof keyData === "string" ? keyData : keyData.id;
+                delete req.body[KEY];
+            }
+            else
+            {
+                if (typeof keyData === "string" && GUID_REGEX.test(keyData))
+                {
+                    try
+                    {
+                        const ASSET_DATA = await NomadSDK.getAsset(keyData);
+                        keyData = {"id": keyData, "description": ASSET_DATA.displayName};
+                    }
+                    catch (error)
+                    {
+                        console.error(`The asset with the ID ${keyData} does not exist.`);
+                        keyData = {"id": keyData};
+                    }
+                }
+
+                PROPERTIES[KEY] = keyData;
+                delete req.body[KEY];
+            }
+        }
+
+        req.body.properties = PROPERTIES;
+
+        const ID = await NomadSDK.updateContent(req.body.masterId, req.body.contentDefinitionId, req.body.properties, req.body.language);
+
+        return res.status(200).json(ID);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/getContent', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT = await NomadSDK.search(null, 0, null, 
+            [
+                {
+                    fieldName: "contentDefinitionId",
+                    operator: "Equals",
+                    values: req.body.contentDefinitionId,
+                },
+                {
+                    fieldName: "masterId",
+                    operator: "Equals",
+                    values: req.body.masterId
+                },
+                {
+                    fieldName: "languageId",
+                    operator: "Equals",
+                    values: JSON.parse(req.body.getContentLanguage).id
+                }
+            ], null, null, null, null, null, null);
+        console.log(JSON.stringify(CONTENT, null, 4));
+        return res.status(200).json(CONTENT.items[0]);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/updateContent', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT = await NomadSDK.search(null, 0, null, 
+            [
+                {
+                    fieldName: "contentDefinitionId",
+                    operator: "Equals",
+                    values: req.body.contentDefinitionId,
+                },
+                {
+                    fieldName: "masterId",
+                    operator: "Equals",
+                    values: req.body.masterId
+                },
+                {
+                    fieldName: "languageId",
+                    operator: "Equals",
+                    values: JSON.parse(req.body.updateContentLanguage).id
+                }
+            ], null, null, null, null, null, null);
+
+        const CONTENT_ID = CONTENT.items[0].id;
+
+        const PROPERTIES = {};
+        const GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+        for (const KEY in req.body)
+        {
+            let keyData = checkString(req.body[KEY]);
+            req.body[KEY] = keyData;
+
+            if (KEY.includes("updateContent"))
+            {
+                let correctedKey = KEY.replace("updateContent", "");
+                correctedKey = correctedKey.charAt(0).toLowerCase() + correctedKey.slice(1);
+                req.body[correctedKey] = typeof keyData === "string" ? keyData : keyData.id;
+                delete req.body[KEY];
+            }
+            else
+            {
+                if (typeof keyData === "string" && GUID_REGEX.test(keyData))
+                {
+                    try
+                    {
+                        const ASSET_DATA = await NomadSDK.getAsset(keyData);
+                        keyData = {"id": keyData, "description": ASSET_DATA.displayName};
+                    }
+                    catch (error)
+                    {
+                        console.error(`The asset with the ID ${keyData} does not exist.`);
+                        keyData = {"id": keyData};
+                    }
+                }
+
+                PROPERTIES[KEY] = keyData;
+                delete req.body[KEY];
+            }
+        }
+
+        req.body.properties = PROPERTIES;
+
+        await NomadSDK.updateContent(CONTENT_ID, req.body.contentDefinitionId, req.body.properties, req.body.language);
+
+        return res.status(200).json(CONTENT_ID);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/deleteContent', upload.none(), async (req, res) => {
+    try
+    {
+        const CONTENT = await NomadSDK.search(null, 0, null, 
+            [
+                {
+                    fieldName: "contentDefinitionId",
+                    operator: "Equals",
+                    values: req.body.contentDefinitionId,
+                },
+                {
+                    fieldName: "masterId",
+                    operator: "Equals",
+                    values: req.body.masterId
+                },
+                {
+                    fieldName: "languageId",
+                    operator: "Equals",
+                    values: JSON.parse(req.body.deleteContentLanguage).id
+                }
+            ], null, null, null, null, null, null);
+
+        const CONTENT_ID = CONTENT.items[0].id;
+
+        await NomadSDK.deleteContent(CONTENT_ID, req.body.contentDefinitionId);
+    }
+    catch (error)
+    {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+function checkString(str)
+{
+    try
+    {
+        return JSON.parse(str);
+    }
+    catch (error)
+    {
+        return str;
+    }
+}
