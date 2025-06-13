79203729fef25e314d43df8d8127fe6cda39c823
Scott <sfaludi@nomad-cms.com>
2025-06-13 05:53:53 -0700

    added share sample

---
+++ share/Readme.md
@@ -0,0 +1,77 @@
+## Prerequisites
+
+- Node package manager (npm).
+
+> 📘 Note
+> 
+> You can download npm [here](https://nodejs.org/en/download).
+
+## Nomad SDK NPM
+
+To learn how to download and setup the nomad sdk npm, go to [Nomad SDK NPM](https://github.com/Nomad-Media/nomad-sdk/tree/main/nomad-sdk-npm).
+
+## Setup
+
+To run the Node application, follow these steps:
+```
+npm install
+npm start
+```
+
+Then open a webpage and go to localhost:4200.
+
+## Nomad SDK Files
+
+In the nomad-sdk/js directory there are two versions of the Nomad SDK. There is the sdk.min.js file which i Minified Version Of the sdk, and the sdk-debug.js file which i Concatenated Version Of the sdk. The sdk-debug file will show you all the parameter documentation and readable code.
+
+## Share
+
+To share an asset, enter relevent information for your share into the fields and click `Submit`.
+
+![](images/share.png)
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Share](https://developer.nomad-cms.com/docs/share)
+
+## Get Share
+
+To get a share, enter the id of the share and click `Submit`.
+
+![](images/get-share.png)
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Get Share](https://developer.nomad-cms.com/docs/get-content)
+
+## Update Share
+
+To update a share, enter the id of the share into the first field. Then enter data into the fields you want to update and click `Submit`.
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Update Shate](https://developer.nomad-cms.com/docs/update-share)
+
+## Share Expire
+
+To expire a share, enter the id of the share and click `Submit`.
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Share Expire](https://developer.nomad-cms.com/docs/share-expire)
+
+## Share Notification
+
+To get the notifications for a sahre, enter the id of the share and the id(s) of the nomad users and/or external users and click `Submit`.
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Share Notification](https://developer.nomad-cms.com/docs/share-notification)
+
+## Delete Share
+
+To delete a share, enter the id of the share and click `Submit`.
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Delete Share](https://developer.nomad-cms.com/docs/delete-share)
---
+++ share/public/share.js
@@ -0,0 +1,123 @@
+import NomadMediaSDK from "@nomad-media/full";
+import config from "../config.js";
+const nomadSdk = new NomadMediaSDK(config);
+
+// Form elements
+const shareForm = document.getElementById("shareForm");
+const getShareForm = document.getElementById("getShareForm");
+const updateShareForm = document.getElementById("updateShareForm");
+const shareExpireForm = document.getElementById("shareExpireForm");
+const shareNotificationForm = document.getElementById("shareNotificationForm");
+const deleteShareForm = document.getElementById("deleteShareForm");
+
+// Create Share
+shareForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(shareForm);
+    console.log(elems);
+
+    const sharedContents = elems.sharedContents ? elems.sharedContents.split(",") : [];
+    const sharedPermissions = elems.sharedPermissions ? elems.sharedPermissions.split(",") : [];
+
+    const result = await nomadSdk.share(
+        elems.assetId,
+        elems.name,
+        sharedContents,
+        { id: elems.sharedDuration },
+        sharedPermissions,
+        elems.sharedType,
+        elems.sharedStatus,
+        elems.sharedDurationInHours,
+        elems.sharedLink,
+        elems.ownerId,
+        elems.expirationDate
+    );
+
+    console.log(result);
+});
+
+// Get Share
+getShareForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(getShareForm);
+
+    const result = await nomadSdk.getShare(elems.shareId);
+    console.log(result);
+});
+
+// Update Share
+updateShareForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(updateShareForm);
+
+    const sharedContents = elems.sharedContents ? elems.sharedContents.split(",") : [];
+    const sharedPermissions = elems.sharedPermissions ? elems.sharedPermissions.split(",") : [];
+    
+    const result = await nomadSdk.updateShare(
+        elems.shareId,
+        elems.assetId,
+        elems.name,
+        sharedContents,
+        { id: elems.sharedDuration },
+        sharedPermissions,
+        elems.sharedType,
+        elems.sharedStatus,
+        elems.sharedDurationInHours,
+        elems.sharedLink,
+        elems.ownerId,
+        elems.expirationDate
+    );
+
+    console.log(result);
+});
+
+// Share Expire
+shareExpireForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(shareExpireForm);
+
+    const result = await nomadSdk.shareExpire(shareId);
+    console.log(result);
+});
+
+// Share Notification
+shareNotificationForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(shareNotificationForm);
+
+    const result = await nomadSdk.shareNotification(elems.shareId, );
+    console.log(result);
+});
+
+// Delete Share
+deleteShareForm.addEventListener("submit", async function (event)
+{
+    event.preventDefault();
+    const elems = getElements(deleteShareForm);
+
+    const result = await nomadSdk.deleteShare(elems.shareId);
+    console.log(result);
+});
+
+// Helper to get form data
+function getElements(form)
+{
+    const elems = {}
+    for (let input of form)
+    {
+        if (input.tagName === "INPUT" || input.tagName === "SELECT")
+        {
+            if (input.type !== "checkbox" || (input.type === "checkbox" && input.checked))
+            {
+                const value = input.value !== "" ? input.value : null;
+                input.id ? elems[input.id] = value : elems[input.name] = value;
+            }
+        }
+    }
+    return elems;
+}
